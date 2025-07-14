373635c3b0605a0343002d5df8ba86d1d05ba118
nexusagro <131743965+nexusagro@users.noreply.github.com>
2025-07-15 03:06:59 +0530

    next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,24 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['buildmarketlk.com', 'www.buildmarketlk.com'],
-    unoptimized: false,
+    domains: [
+      "hebbkx1anhila5yf.public.blob.vercel-storage.com",
+      "masterbuilders.s3.ap-southeast-1.amazonaws.com",
+      "example.com",
+      "www.techbuilders.com",
+      "buildmarketbucket.s3.ap-southeast-1.amazonaws.com"
+    ],
+   
   },
-  // Ensure static files are copied
-  trailingSlash: false,
-  // Add headers for static assets
-  async headers() {
-    return [
-      {
-        source: '/frames/:path*',
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'public, max-age=31536000, immutable',
-          },
-        ],
-      },
-    ]
-  },
-}
-export default nextConfig
+};
+
+export default nextConfig;
