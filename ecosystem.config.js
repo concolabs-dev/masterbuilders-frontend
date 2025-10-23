c3cbce7cf28b818bc8cc73e69c1877bbb884ac03
anupaperera112 <anupaperera112@gmail.com>
2025-10-23 22:21:24 +0530

    eco system add

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,6 +4,9 @@ module.exports = {
       name: "frontend",
       script: "npm",
       args: "start",
+      cwd: "/home/ubuntu/masterbuilders-frontend",
+      node_args: "-r dotenv/config",
+      args: "--dotenv_config_path=/home/ubuntu/masterbuilders-frontend/.env.test",
       env_file: ".env.test",
     }
   ]
