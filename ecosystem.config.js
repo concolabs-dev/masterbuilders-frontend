4388457ffc6f4a418a8e8834bbc13e74f6b21f2d
anupaperera112 <anupaperera112@gmail.com>
2025-10-31 13:48:06 +0530

    ss

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,8 +4,7 @@ module.exports = {
       name: "frontend",
       script: "npm",
       args: "start", // The script command for npm
-      cwd: "/home/ubuntu/masterbuilders-frontend",
-      env_file: ".env.test", // The correct PM2 way to load this file
+      cwd: "/home/ubuntu/masterbuilders-frontend"
     }
   ]
 };
